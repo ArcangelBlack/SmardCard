65c20573e260b93ab962fc266c7c0205a2d36b5d
Abdel Jaouhari <ajaouhari@itteria.com>
2018-06-01 12:18:29 +0200

    Añadida la función con la que se puede firmar el documento.

--- SmartCardWebApplication/Controllers/HomeController.cs
+++ SmartCardWebApplication/Controllers/HomeController.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
-using SmartCardWebApplication.Models;
 using SmartCardWebApplication.Models.ViewModel;
 
 namespace SmartCardWebApplication.Controllers
@@ -29,6 +26,7 @@ namespace SmartCardWebApplication.Controllers
         {
             var smartCardCerts = new List<X509Certificate2>();
             var myStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
             myStore.Open(OpenFlags.ReadOnly);
             foreach (X509Certificate2 cert in myStore.Certificates)
             {
@@ -84,15 +82,16 @@ namespace SmartCardWebApplication.Controllers
             if (file != null && file.ContentLength > 0)
                 try
                 {
-                    string path = Path.Combine(Server.MapPath("~/Documents"),
-                                               Path.GetFileName(file.FileName));
+                    string path = Path.Combine(Server.MapPath("~/Documents"), Path.GetFileName(file.FileName));
+
                     file.SaveAs(path);
 
-                    SignHashed("mariano", "mariano", true, path);
+                    X509Certificate2 certificado = GetCert();
+                    
+
+                    SignHashed(certificado.FriendlyName, "mariano", true, path);
 
                     ViewBag.Message = "File uploaded successfully";
-
-
 
                 }
                 catch (Exception ex)
@@ -125,70 +124,97 @@ namespace SmartCardWebApplication.Controllers
             return View();
         }
 
-        public static void SignHashed(string SigReason, string SigContact, bool visible, string path)
+        public void SignHashed(string SigReason, string SigContact, bool visible, string path)
         {
+            string input = path;
+            string output = "C:\\Users\\ajaouhari\\Documents\\Proyectos\\ODP\\SmartCardWebApplication\\Documents\\Document-Signed.pdf";
 
-            using (MemoryStream pdfData = new MemoryStream())
+
+            using (MemoryStream xd = new MemoryStream())
             {
-                //string template = @"C:\Documents and Settings\mshyavitz\My Documents\Visual Studio 2008\Projects\WFMO.RADS.Web\print\sf52.pdf";
-                using (PdfReader pdfReader = new PdfReader(path))
+
+
+                using (PdfReader reader = new PdfReader(input))
+                //create PdfStamper object to write to get the pages from reader 
+                using (PdfStamper stamper = new PdfStamper(reader, new FileStream(output, FileMode.Create)))
                 {
-                    using (PdfStamper st = PdfStamper.CreateSignature(pdfReader, pdfData, '\0', null, true))
-                    {
+                    //select two pages from the original document
+                    reader.SelectPages("1"+"-"+reader.NumberOfPages);
 
-                        PdfSignatureAppearance sap = st.SignatureAppearance;
+                    //gettins the page size in order to substract from the iTextSharp coordinates
+                    var pageSize = reader.GetPageSize(reader.NumberOfPages);
 
-                        sap.Reason = SigReason;
-                        sap.Contact = SigContact;
+                    // PdfContentByte from stamper to add content to the pages over the original content
+                    PdfContentByte pbover = stamper.GetOverContent(reader.NumberOfPages);
 
-                        if (visible)
-                            sap.SetVisibleSignature(new iTextSharp.text.Rectangle(100, 100, 250, 150), 1, null);
+                    //add content to the page using ColumnText
+                    Font font = new Font();
+                    font.Size = 10;
 
-                        st.FormFlattening = true;
-                        st.Writer.CloseStream = false;
-                        st.Close();
+                    //setting up the X and Y coordinates of the document
+                    int x = 150;
+                    int y = 200;
 
-                        //            }
-                        //using (PdfStamper pdfStamper = new PdfStamper(pdfReader, pdfData))
-                        //{
-                        //    AcroFields pdfFormFields = pdfStamper.AcroFields;
+                    y = (int)(pageSize.Height - y);
 
-                        //    // set fields here. 
-
-                        //    pdfStamper.FormFlattening = true;
-                        //    pdfStamper.Writer.CloseStream = false;
-                        //    pdfStamper.Close();
-                        //}
-                    }
+                    ColumnText.ShowTextAligned(pbover, Element.ALIGN_CENTER, new Phrase(SigReason, font), x, y, 0);
                 }
-
-
-
-
-                //    PdfReader reader = new PdfReader(path);
-                //    //Activate MultiSignatures
-                //    using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-                //    {
-                //        using (MemoryStream ms = new MemoryStream())
-                //        {
-                //            using (PdfStamper st = PdfStamper.CreateSignature(reader, ms, '\0', null, true))
-                //            {
-
-                //                PdfSignatureAppearance sap = st.SignatureAppearance;
-
-                //                sap.Reason = SigReason;
-                //                sap.Contact = SigContact;
-
-                //                if (visible)
-                //                    sap.SetVisibleSignature(new iTextSharp.text.Rectangle(100, 100, 250, 150), 1, null);
-
-                //                st.Close();
-                //            }
-                //        }
-                //    }
-
-                //}
             }
         }
+
+        #region Cert
+
+        public X509Certificate2 GetCert()
+        {
+            var smartCardCerts = new List<X509Certificate2>();
+            var myStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+            myStore.Open(OpenFlags.ReadOnly);
+            foreach (X509Certificate2 cert in myStore.Certificates)
+            {
+                try
+                {
+                    if (!cert.HasPrivateKey) continue;// not smartcard for sure
+
+                    if (string.IsNullOrEmpty(cert.FriendlyName))
+                    {
+                        continue;
+                    }
+                    if (cert.PrivateKey is RSACryptoServiceProvider)
+                    {
+                        var rsa = cert.PrivateKey as RSACryptoServiceProvider;
+                        if (rsa == null) continue; // not smart card cert again
+                        if (rsa.CspKeyContainerInfo.HardwareDevice) // sure - smartcard
+                        {
+                            // inspect rsa.CspKeyContainerInfo.KeyContainerName Property
+                            // or rsa.CspKeyContainerInfo.ProviderName (your smartcard provider, such as 
+                            // "Schlumberger Cryptographic Service Provider" for Schlumberger Cryptoflex 4K
+                            // card, etc
+                            Console.WriteLine(cert.FriendlyName);
+                            Console.WriteLine(cert.SerialNumber);
+                            Console.WriteLine(cert.Issuer);
+
+                            //rsa.SignData(); // to confirm presence of private key - to finally authenticate
+
+                            return cert;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Fallo");
+                    continue;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region add digital signature to PDF document
+
+
+        #endregion
     }
 }
